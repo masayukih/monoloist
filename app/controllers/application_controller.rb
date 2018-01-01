eac717f55e18efc04640fe46e58857ba69b8479b
masayuki.higashi <masayuki.h1109@gmail.com>
2018-01-01 07:50:08 +0000

    want

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,20 @@ class ApplicationController < ActionController::Base
   
   private
   
+  def read(result)
+    code = result['itemCode']
+    name = result['itemName']
+    url = result['itemUrl']
+    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
+
+    return {
+      code: code,
+      name: name,
+      url: url,
+      image_url: image_url,
+    }
+  end
+  
   def require_user_logged_in
   	unless logged_in?
   		redirect_to login_url
